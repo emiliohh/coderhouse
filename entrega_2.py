b32c396cadb0036675faf29339dde5bf8249b423
emiliohh <ehopph@gmail.com>
2023-09-04 21:16:46 -0300

    prier commit

--- entrega_2.py
+++
@@ -1,5 +0,0 @@
-'''
-Documentation, License etc.
-
-@package entrega_2
-'''
---
+++ main.py
@@ -0,0 +1,10 @@
+from paquete1.modulo1 import Cliente
+from paquete1.modulo2 import *
+
+cliente1 = Cliente("Emilio","sin@mail.co",30,["tecnología","moda","farmacia"])
+
+cliente1.comprar("Laptop","Walmart")
+cliente1.devlolver("PlayStaion 5","Sony")
+print(cliente1)
+
+
